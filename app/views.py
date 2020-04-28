b0f0f5fade19f37d045e90ae75c9541ff836a3c5
Dolphine Atieno <awuordolphine65@gmail.com>
2020-04-28 06:23:48 +0300

    add an article app. route

--- app/views.py
+++ app/views.py
@@ -8,3 +8,10 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route('/articles/<id>')
+def news(id):
+    '''
+    view news articles page that is detailed
+    '''
+    return render_template('articles.html')
