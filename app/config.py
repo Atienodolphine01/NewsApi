7dfa38f07e627cb319196a5c4395663861c009f7
Dolphine Atieno <awuordolphine65@gmail.com>
2020-04-28 05:44:50 +0300

    deleted everything to start afresh

--- app/config.py
+++ app/config.py
@@ -1,36 +1,3 @@
-import os
-
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey'
-    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
-
-    config_options = {
-    "production":ProdConfig,
-    "development": DevConfig
-}
+    
